83f9dc7ec593bafa81cb4e57c2ead180ca4845cf
colinskorir <korircoll20@gmail.com>
2025-03-11 09:14:47 +0300

    files added and modified

---
+++ src/00-objectball.js
@@ -0,0 +1,123 @@
+function gameObject {
+    return {
+      home: {
+        teamName: "Brooklyn Nets",
+        colors: ["Black", "White"],
+        players: {
+          "Alan Anderson": {
+            number: 0,
+            shoe: 16,
+            points: 22,
+            rebounds: 12,
+            assists: 12,
+            steals: 3,
+            blocks: 1,
+            slamDunks: 1,
+          },
+          "Reggie Evans": {
+            number: 30,
+            shoe: 14,
+            points: 12,
+            rebounds: 12,
+            assists: 12,
+            steals: 12,
+            blocks: 12,
+            slamDunks: 7,
+          },
+          "Brook Lopez": {
+            number: 11,
+            shoe: 17,
+            points: 17,
+            rebounds: 19,
+            assists: 10,
+            steals: 3,
+            blocks: 1,
+            slamDunks: 15,
+          },
+          "Mason Plumlee": {
+            number: 1,
+            shoe: 19,
+            points: 26,
+            rebounds: 12,
+            assists: 6,
+            steals: 3,
+            blocks: 8,
+            slamDunks: 5,
+          },
+          "Jason Terry": {
+            number: 31,
+            shoe: 15,
+            points: 19,
+            rebounds: 2,
+            assists: 2,
+            steals: 4,
+            blocks: 11,
+            slamDunks: 1,
+          },
+        },
+      },
+      away: {
+        teamName: "Charlotte Hornets",
+        colors: ["Turquoise", "Purple"],
+        players: {
+          "Jeff Adrien": {
+            number: 4,
+            shoe: 18,
+            points: 10,
+            rebounds: 1,
+            assists: 1,
+            steals: 2,
+            blocks: 7,
+            slamDunks: 2,
+          },
+          "Bismak Biyombo": {
+            number: 0,
+            shoe: 16,
+            points: 12,
+            rebounds: 4,
+            assists: 7,
+            steals: 7,
+            blocks: 15,
+            slamDunks: 10,
+          },
+          "DeSagna Diop": {
+            number: 2,
+            shoe: 14,
+            points:
+  }
+  
+  console.log(gameObject);
+  
+  function homeTeamName() {
+    return gameObject()["home"]["teamName"]
+  }
+  
+  console.log(homeTeamName());
+
+  function numPointsScored(playersName) {
+    return gameObject()["home"]["players"]["Alan Anderson"]["points"]
+
+  }
+  function shoeSize(playersName) {
+
+  }
+  function teamColors(teamName) {
+
+  }
+  function teamNames() {
+    return [gameObject()["home"]["teamName"], gameObject()["away"]["teamName"]]
+  }
+  function playerNumbers(teamName) {
+    return gameObject()[teamName]["players"]["number"]
+
+  }
+  function playerStats(playersNames) {
+    return gameObject()["home"]["players"]["Alan Anderson"]
+
+  }
+  function bigShoeRebounds() {
+    return 12
+  }
+  function doesLongNameStealAton() {
+    return false
+  }
